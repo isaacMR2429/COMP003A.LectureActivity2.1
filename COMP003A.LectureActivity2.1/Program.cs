084e921fc668560464af04ff0e5296a76bffb4ea
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-01-21 16:07:08 -0800

    added the sum of num1 and num 2 and getting their average

--- COMP003A.LectureActivity2.1/Program.cs
+++ COMP003A.LectureActivity2.1/Program.cs
@@ -12,6 +12,11 @@
 
             double num1 = double.Parse(input1);
             double num2 = double.Parse(input2);
+
+            double sum = num1 + num2;
+            double average = (num1 + num2) / 2;
+
+
         }
     }
 }
